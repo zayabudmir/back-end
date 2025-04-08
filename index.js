f397a2f9eb4a2609398933dcdcc09738a061f19f
24LP9962 24LP9962 <24LP9962@G302-CC0111.local>
2025-04-08 12:41:22 +0800

    delete, update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import express, { json } from "express";
+import express, { json, request, response } from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const port = 8080;
 
@@ -6,32 +7,79 @@ const app = express();
 
 app.use(json());
 
-app.get("/", (req, res) => {});
+let users = [];
 
-app.get("/user", (req, res) => {
-  const { username, password } = req.body;
+app.post("/user/create", (request, response) => {
+  const { username, gender, age, email } = request.body;
+  console.log(username, gender, age, email, "body");
+  users.push({
+    username,
+    gender,
+    age,
+    email,
+    id: uuidv4(),
+  });
 
-  if (username.length < 5) {
-    res
-      .status(400)
-      .send({
-        success: false,
-        message: "username zaaval 5 deesh temdegt orson bh ysto",
-      })
-      .end();
-  }
+  console.log(users, "users");
+  response.send({
+    success: true,
+    message: "Done",
+  });
+});
 
-  if (password.length < 8) {
-    res
-      .status(400)
-      .send({
-        success: false,
-        message: "password zaaval 8 deesh temdegt orson bh ysto",
-      })
-      .end();
-  }
+app.get("/users", (_, response) => {
+  response.send(users);
+});
 
-  res.send({ sucess: true }).end();
+app.get("/user", (request, response) => {
+  const { id } = request.body;
+  console.log(request.body, "request");
+  const user = users.find((user) => {
+    return user.id == id;
+  });
+
+  console.log(user);
+  response.send(user);
+
+  // response.send(user[0]);
+
+  // const filter = users.filter((value) => value.email === email);
+
+  // response.send(filter);
+});
+
+app.delete("/user/delete", (request, response) => {
+  const { id } = request.body;
+  users = users.filter((user) => user.id !== id);
+  response.send({
+    success: true,
+    message: "removed",
+  });
+});
+
+app.put("/user/update", (req, res) => {
+  const { id, username, email, gender, age } = req.body;
+
+  users.map((user) => {
+    if (user.id === id) {
+      user.username = username;
+      user.email == email;
+      user.gender == gender;
+      user.age == age;
+    }
+    return;
+  });
+
+  // const { id } = request.body;
+  // users = users.filter((user) => user.id == id);
+  response.send({
+    success: true,
+    message: "uo",
+  });
+});
+
+app.get("/", (request, response) => {
+  response.send("hello world");
 });
 
 app.listen(port, () => {
